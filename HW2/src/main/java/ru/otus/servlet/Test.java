1b24233badc7f798ce0ca14f354fddcf9109a8d0
Molokhov Sergey <smolokhov@gmail.com>
2018-01-15 16:24:01 +0300

    HW2 добавлен класс для тестирования persist и указана проблемная версия hibernate

---
+++ HW2/src/main/java/ru/otus/servlet/Test.java
@@ -0,0 +1,36 @@
+package ru.otus.servlet;
+
+import ru.otus.model.City;
+import ru.otus.model.Employee;
+import ru.otus.util.LocalEntityManagerFactory;
+
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html; charset=UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        EntityManager em = LocalEntityManagerFactory.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            City city = new City();
+            city.setId(10000L);
+            em.persist(city);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+
+    }
+}
